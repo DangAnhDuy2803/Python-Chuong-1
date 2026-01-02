382183b5fdfa0823ab634017dc962f1169ece01f
Dang Anh Duy <163238863+DangAnhDuy2803@users.noreply.github.com>
2026-01-02 16:36:21 +0700

    btc1

--- Ex9.py
+++ Ex9.py
@@ -2,10 +2,9 @@
 # (non-repeating) elements of the list.
 lst = [2,4,2,5,7,1,2,1,8,9,10,2,10,4,3,5]
 print(lst)
-
 reducedlst=[] #Create an empty list
-
 for i in range (0,len(lst)):
     if lst[i] not in reducedlst:
         reducedlst.append(lst[i])
 print(reducedlst)
+
