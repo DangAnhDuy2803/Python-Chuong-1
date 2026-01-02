0217205330f0971e7bb6c7c5b2daccf6453e9e67
Dang Anh Duy <163238863+DangAnhDuy2803@users.noreply.github.com>
2026-01-02 16:37:56 +0700

    btc1

--- Ex14.py
+++ Ex14.py
@@ -4,6 +4,7 @@
 lstA = [1,2,3,4,5,6,7,8,9]
 lstB = [4,2,6,9,10,11,12,13,15,16]
 lstC =[]
+
 #ket hop lstA va lstB chi lay nhung gia tri ko giong nhau trong ca 2
 # for x in lstA:
 #     if x not in lstB:
@@ -22,4 +23,5 @@ for x in lstA:
 for x in lstB:
     if x not in lstC:
         lstC.append(x)
+
 print(lstC)
